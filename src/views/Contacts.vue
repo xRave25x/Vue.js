ab8a7adfbbcb4d4ab8ef73fc3acbdb9b09640201
Andrei Demichev <andyuson24@gmail.com>
2024-03-06 14:15:50 +0200

    create lesson10(validation)

--- src/views/Contacts.vue
+++ src/views/Contacts.vue
@@ -16,10 +16,10 @@
                     <div class="col col-12 col-lg-6 offset-0 offset-lg-3">
                         <div class="title mt-5">Tell us about your tastes</div>
                         <img class="beanslogo mt-5" src="@/assets/logo/Beans_logo_dark.svg" alt="Beans logo">
-    
+
                         <form @submit.prevent="submit" action="#" class="mt-5">
                             <div class="form-group row">
-                                <div class="col col-12 col-sm-3 d-flex align-items-center">
+                                <div class="col col-12 col-sm-3 d-flex align-items-start">
                                     <label for="name-input" class="mb-0">
                                         Name
                                         <span style="color: red;">*</span>
@@ -27,49 +27,65 @@
                                 </div>
                                 <div class="col col-12 col-sm-9">
                                     <input 
-                                        v-model="form.name"
-                                        type="text" 
-                                        class="form-control" 
-                                        id="name-input"
+                                    v-model="v$.name.$model" 
+                                    type="text" 
+                                    class="form-control" 
+                                    id="name-input"
                                     >
+                                    <span 
+                                    v-for="error in v$.name.$errors"
+                                    :key="error.$uid"
+                                    >
+
+                                    {{ error.$message }}
+
+                                    </span>
                                 </div>
                             </div>
-    
+
                             <div class="form-group row">
-                                <div class="col col-12 col-sm-3 d-flex align-items-center">
+                                <div class="col col-12 col-sm-3 d-flex align-items-start">
                                     <label for="email-input" class="mb-0">
                                         E-mail
                                         <span style="color: red;">*</span>
                                     </label>
                                 </div>
                                 <div class="col col-12 col-sm-9">
-                                    <input
-                                        v-model="form.email"
-                                        type="email" 
-                                        class="form-control" 
-                                        id="email-input"
+                                    <input 
+                                    v-model="v$.email.$model" 
+                                    type="email" 
+                                    class="form-control" 
+                                    id="email-input"
+                                    
                                     >
+                                    <span 
+                                    v-for="error in v$.email.$errors"
+                                    :key="error.$uid"
+                                    >
+
+                                    {{ error.$message }}
+                                    </span>
                                 </div>
                             </div>
-    
+
                             <div class="form-group row">
-                                <div class="col col-12 col-sm-3 d-flex align-items-center">
+                                <div class="col col-12 col-sm-3 d-flex align-items-start">
                                     <label for="phone-input" class="mb-0">
                                         Phone
                                     </label>
                                 </div>
                                 <div class="col col-12 col-sm-9">
-                                    <input
-                                        v-model="form.phone"
-                                        type="tel" 
-                                        class="form-control" 
-                                        id="phone-input"
+                                    <input 
+                                    v-model="phone" 
+                                    type="tel" 
+                                    class="form-control" 
+                                    id="phone-input"
                                     >
                                 </div>
                             </div>
-    
+
                             <div class="form-group row textarea">
-                                <div class="col col-12 d-flex justify-content-center">
+                                <div class="col col-12 d-flex justify-content-start">
                                     <label for="pmessage" class="mb-3 mt-3 text-center">
                                         Your message
                                         <span style="color: red;">*</span>
@@ -77,16 +93,23 @@
                                 </div>
                                 <div class="col col-12">
                                     <textarea 
-                                        v-model="form.message"
-                                        class="form-control" 
-                                        name="message" 
-                                        id="message" 
-                                        rows="5"
-                                        placeholder="Leave your comments here"
+                                    v-model="v$.message.$model" 
+                                    class="form-control" 
+                                    name="message" 
+                                    id="message"
+                                    rows="5" 
+                                    placeholder="Leave your comments here"
                                     ></textarea>
+                                    <span 
+                                    v-for="error in v$.message.$errors"
+                                    :key="error.$uid"
+                                    >
+
+                                    {{ error.$message }}
+                                    </span>
                                 </div>
                             </div>
-    
+
                             <div class="row">
                                 <div class="col">
                                     <button class="btn btn-outline-dark send-btn">Send us</button>
@@ -103,21 +126,38 @@
 <script>
 import NavBarComponent from '@/components/NavBarComponent.vue';
 
+import  useVuelidate  from '@vuelidate/core'
+import { required, email, maxLength } from '@vuelidate/validators'
+import { helpers } from '@vuelidate/validators'
+import { minLength } from '../validators/minLength';
+
 export default {
+    setup() {
+        return { v$: useVuelidate() }
+    },
     data() {
         return {
-            form: {
-                name: '',
-                email: '',
-                phone: '',
-                message: ''
-            }
+            name: '',
+            email: '',
+            phone: '',
+            message: ''
         }
     },
-    components: {NavBarComponent},
+    validations() {
+        return {
+            name: { required },
+            email: { required, email },
+            phone: { },
+            message: { required, maxLength: maxLength(20), minLength: helpers.withMessage('this value min 5', minLength) }
+        }
+    },
+    components: { NavBarComponent },
     methods: {
-        submit() {
-            console.log(this.form);
+        async submit() {
+            const isFormCorrect = await this.v$.$validate()
+        if (!isFormCorrect) return
+
+
         }
     }
 }
