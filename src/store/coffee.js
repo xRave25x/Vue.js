d56b1db5cc95c7589f43b506f47b2fc94d8694eb
Andrei Demichev <andyuson24@gmail.com>
2024-03-07 10:28:29 +0200

    Create lesson12(filtres)

--- src/store/coffee.js
+++ src/store/coffee.js
@@ -2,15 +2,30 @@
 const coffee = {
     state: {
         coffee: [],
+        searchValue: '',
+        sortValue: '',
     },
     mutations: {
       setCoffeeData(state, data) {
         state.coffee = data
-      }
+      },
+      setSearchValue(state, value) {
+        state.searchValue = value
+      },
+      setSortValue(state, value) {
+        state.sortValue = value
+      },
+      
     },
     actions: {
       setCoffeeData({commit}, data) {
         commit('setCoffeeData', data)
+      },
+      setSearchValue({commit}, value) {
+        commit('setSearchValue', value)
+      },
+      setSortValue({commit}, value) {
+        commit('setSortValue', value)
       }
     },
     getters: {
@@ -21,7 +36,10 @@ const coffee = {
           return (id) => {
             return state.coffee.find((card) => card.id === +id)
           }
-        }
+        },
+        getSearchValue(state) {
+          return state.searchValue
+      },
     }
 }
 export default coffee
--- src/views/OurCoffeeView.vue
+++ src/views/OurCoffeeView.vue
@@ -52,16 +52,17 @@
                 type="text"
                 placeholder="start typing here..."
                 class="shop__search-input"
+                @input="onSearch($event)"
               />
             </form>
           </div>
           <div class="col-lg-4">
             <div class="shop__filter">
-              <div class="shop__filter-label">Or filter</div>
+              <div class="shop__filter-label" @click="onSort('')">Or filter</div>
               <div class="shop__filter-group">
-                <button class="shop__filter-btn">Brazil</button>
-                <button class="shop__filter-btn">Kenya</button>
-                <button class="shop__filter-btn">Columbia</button>
+                <button class="shop__filter-btn" @click="onSort('Brazil')">Brazil</button>
+                <button class="shop__filter-btn" @click="onSort('Kenya')">Kenya</button>
+                <button class="shop__filter-btn" @click="onSort('Columbia')">Columbia</button>
               </div>
             </div>
           </div>
@@ -77,7 +78,6 @@
                 :card="card"
                 @onNavigate="navigate"
               />
-              <!-- /our-coffee/item -->
             </div>
           </div>
         </div>
@@ -90,12 +90,21 @@
 import NavBarComponent from "@/components/NavBarComponent.vue";
 import ProductCard from "@/components/ProductCard.vue";
 import { navigate } from "@/mixins/navigate";
+import debounce from 'debounce';
 
 export default {
   components: { NavBarComponent, ProductCard },
   computed: {
     coffee() {
       return this.$store.getters['getCoffee']
+    },
+    searchValue: {
+      set(value) {
+        this.$store.dispatch("setSearchValue", value)
+      },
+      get() {
+        return this.$store.getters['getSearchValue']
+      }
     }
   },
   data() {
@@ -106,10 +115,24 @@ export default {
   mixins: [navigate],
   mounted() {
     fetch('http://localhost:3000/coffee')
-    .then(res => res.json())
-    .then(data => {
+    .then((res) => res.json())
+    .then((data) => {
       this.$store.dispatch("setCoffeeData", data)
     })
+  },
+  methods: {
+    onSearch: debounce(function(event) {
+      this.onSort(event.target.value)
+    },500),
+    onSort(value) {
+      fetch(`http://localhost:3000/coffee?q=${value}`)
+      .then((res) => res.json())
+      .then((data) => {
+      this.$store.dispatch("setCoffeeData", data)
+    })
+    }
   }
 };
 </script>
+
+
